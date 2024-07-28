5e67d8612cc7e75723aed3509375f9fad1bfecac
Julimar Jr <julimargomesjr@hotmail.com>
2024-07-28 13:40:55 -0300

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import MatchRouter from "./src/routers/match.router.js"
 import dotenv from 'dotenv'
 import * as CacheHelper from "./src/helpers/cache.helper.js"
@@ -6,6 +7,8 @@ import * as CacheHelper from "./src/helpers/cache.helper.js"
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json({ extends: false }));
 
